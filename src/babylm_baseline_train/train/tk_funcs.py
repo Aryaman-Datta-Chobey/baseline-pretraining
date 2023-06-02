6a0e550f000ffc5781a4cb7ea3f1a896013a2676
Aryaman-Datta-Chobey <97906694+Aryaman-Datta-Chobey@users.noreply.github.com>
2023-06-02 11:39:03 -0700

    Update tk_funcs.py
    hard coded update

--- src/babylm_baseline_train/train/tk_funcs.py
+++ src/babylm_baseline_train/train/tk_funcs.py
@@ -2,9 +2,23 @@ import os
 import pdb
 import setuptools
 import torch
-
+#base libraries for deep learning and NLP , data wrangling 
+import torch 
+import transformers 
+import re
+import tokenizers
+from tokenizers import (
+    normalizers, #1
+    Tokenizer, 
+    pre_tokenizers, #2
+    models, #3
+    trainers,#3
+    processors, #4
+    decoders, #5
+)
+from transformers import PreTrainedTokenizerFast
 from transformers import AutoTokenizer
-from transformers import GPT2Tokenizer
+#from transformers import GPT2Tokenizer
 
 
 def get_gpt2_tokenizer_func(model_name='gpt2'):
@@ -20,7 +34,11 @@ def get_roberta_tokenizer_func(model_name="roberta-base"):
 
 def get_tokenizer_func(opt_model_size='125m'):
     model_name = f"facebook/opt-{opt_model_size}"
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    tokenizer = Tokenizer(models.BPE())
+    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+    trainer = trainers.BpeTrainer(vocab_size=50272, special_tokens=["<|endoftext|>"])
+    tokenizer.train(["/home/achobey/data/babyLM/processed/train.txt"], trainer=trainer)#HARDCODED FOR NOW
+    #tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     tokenizer.add_bos_token = False
     tokenizer.add_special_tokens(
             {
